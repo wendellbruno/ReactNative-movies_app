30e5d5879838104de2aa987b39b3be0d34e479f4
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-11-16 15:09:25 -0300

    initial commit
    initial commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Roboto_700Bold, Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import theme from './src/global/styles/theme';
 
 export default function App() {
+
+  let [fontsLoaded, fontError] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,7 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.colors.background,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+    colors: {
+      background: '#2C2C2C',
+      colorYellow: '#FFB039',
+      colorWhite: '#FFF'
+    },
+    fonts:{
+      regular: 'Roboto_400Regular',
+      medium: 'Roboto_500Medium',
+      bold: 'Roboto_700Bold'
+    }
+  };
+  
+  export default theme;
