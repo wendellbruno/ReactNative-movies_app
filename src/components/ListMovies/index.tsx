ba42fec0f110d35a98a78eb4be38f8008c6c0384
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-11-20 10:32:18 -0300

    pages movies done
    pages movies done

--- src/components/ListMovies/index.tsx
+++ src/components/ListMovies/index.tsx
@@ -1,34 +1,57 @@
-import React from 'react';
-import { View, FlatList, Text } from 'react-native';
-import { useQuery } from 'react-query';
-import { getMoviesDiscover } from '../../services';
-import { LinearGradient } from 'expo-linear-gradient';
+import React from "react";
+import { View, FlatList, Text, SafeAreaView, Image } from "react-native";
+import { useQuery } from "react-query";
+import { getMoviesDiscover } from "../../services";
+import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
 
-
-import { styles } from './styles';
+import { styles } from "./styles";
 
 export const ListMovies: React.FC = () => {
-
-  const {data}= useQuery({
-    queryKey: ['MovieDiscover'],
-    queryFn: getMoviesDiscover
-  })
+  const { data } = useQuery({
+    queryKey: ["MovieDiscover"],
+    queryFn: getMoviesDiscover,
+  });
   return (
-    <>
-    <View style={styles.containerHeader}></View>
-    <FlatList
-    data={data}
-    renderItem={({item }) => (
-      <View style={styles.container}>
-        <LinearGradient
-        colors={['#818181', 'transparent']}
-        >
-        <Text>{item.title}</Text>
-        </LinearGradient>
-
+    <SafeAreaView 
+    style={{flex:1}}>
+    <View style={styles.container}>
+      <View style={styles.containerList}>
+        <FlatList
+          data={data}
+          ListHeaderComponent={() => (
+            <View style={styles.containerHeader}>
+              <MaterialCommunityIcons
+                name="movie-open-outline"
+                style={styles.styledIcon}
+                />
+            </View>
+          )}
+          showsVerticalScrollIndicator = {false}
+          ItemSeparatorComponent={() => <View style={{marginBottom: 20}} />}
+          renderItem={({ item }) => (
+            <View style={styles.containerCard}>
+              <View style={styles.containerImgMovie}>
+              <Image 
+              style={styles.imgMovie}
+              source={{uri: `https://image.tmdb.org/t/p/original/${item.poster_path}`}} />
+              </View>
+              <View style={styles.containerDetail}>
+                <Text style={styles.titleDetail}>
+                  {item.title}
+                </Text>
+                <Text style={styles.subtileDetail}>
+                  {item.release_date}
+                </Text>
+              </View>
+              <View style={styles.containerAvarege}>
+              <Text>{item.vote_average.toFixed(1)}</Text>
+                <AntDesign name="star" />
+              </View>
+            </View>
+          )}
+        />
       </View>
-    )}
-    />
-    </>
-  )
-}
+    </View>
+              </SafeAreaView>
+  );
+};
--- src/components/ListMovies/styles.ts
+++ src/components/ListMovies/styles.ts
@@ -8,14 +8,69 @@ export const styles = StyleSheet.create({
         alignItems: 'center'
     },
     containerHeader:{
-        flex: 1
+        flexDirection: 'row',
+        marginBottom: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    styledIcon:{
+        color: theme.colors.colorYellow,
+        fontSize: 60
     },
     containerList:{
-
+        width: '90%',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     containerCard:{
-    width: '80%',
     height: 122,
-    backgroundColor: '#818181',
+    width: 330,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingLeft: 15,
+    gap: 10,
+    flexDirection: 'row',
+    borderRadius: 25,
+    backgroundColor: 'rgba(129,129,129,0.3)',
+    },
+    containerImgMovie: {
+        width: 67,
+        height: 90,
+        borderRadius: 10,
+        overflow: 'hidden'
+    },
+    imgMovie:{
+        width: 67,
+        height: 90,
+        resizeMode: 'cover',
+    },
+    containerDetail:{
+        maxWidth: '70%',
+        gap: 5,
+       
+    },
+    titleDetail:{
+      fontSize: 14,
+      fontFamily: theme.fonts.bold,
+      color: theme.colors.colorWhite
+    },
+    subtileDetail:{
+        fontSize: 12,
+        fontFamily: theme.fonts.regular,
+        color: theme.colors.colorGray
+    },
+    containerAvarege: {
+        width: 34,
+        height: 50,
+        gap: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.colors.colorYellow,
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        borderTopRightRadius: 25,
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,
     }
 });
